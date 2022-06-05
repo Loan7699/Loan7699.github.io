77f31306a756acf0af22496841ad2f9a06c72c0a
Loan7699 <tranphuongloan7699@gmail.com>
2022-06-06 00:15:15 +0700

    add homework_03

---
+++ react/day_02/homework_03/main.js
@@ -0,0 +1,43 @@
+// Bài 3: Cho 1 mảng các số nguyên, trả về bộ ba các số sao cho i#j#k sao cho num[i] + num[j] + num[k] == 0;
+
+function threeSum(array) {
+    // sắp xếp mảng các số
+    array.sort(function (a, b) { return a - b; });
+    // khai báo mảng chứa bộ 3 số và mảng sau khi tìm i,j,k bằng rỗng
+    let triplets = [];
+    let newArray = [];
+
+    for (let i = 0; i < array.length - 2; i++) {
+        let j = i + 1; //j sau i
+        let k = array.length - 1; // k là chỉ số cuối mảng
+
+        while (j < k) {
+            if (array[i] + array[j] + array[k] == 0) {
+                // thỏa mãn Đk thì thêm gtri vào mảng bộ ba số
+                triplets.push(array[i]);
+                triplets.push(array[j]);
+                triplets.push(array[k]);
+
+                // kiểm tra nếu mảng sau ko chứa mảng bộ ba thì thêm bộ ba vào mảng đó
+                if (newArray.includes(triplets) == false) {
+                    newArray.push(triplets);
+                    // gán lại giá trị mảng
+                    triplets = [];
+                }
+
+                j++;
+                k--;
+            }
+
+            else
+                j++;
+        }
+    }
+
+    return newArray;
+}
+
+console.log(threeSum([-1,0,1,2,-1,-4]));
+console.log(threeSum([]));
+console.log(threeSum([0]));
+console.log(threeSum([-12, 3, 6, 1, 6, -9]));
