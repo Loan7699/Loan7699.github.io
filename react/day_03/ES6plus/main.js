f100dab88b9b6ad91d777dd0d55a8db38963751c
Loan7699 <tranphuongloan7699@gmail.com>
2022-06-15 05:14:16 +0700

    add_day5_reactJS

--- react/day_03/ES6plus/main.js
+++ react/day_03/ES6plus/main.js
@@ -132,12 +132,14 @@ var name = "Javascript";
 var price = 1000;
 
 // value: tạo ra 1 obj bao gồm thông tin 2 biến trên
+// d/n key: value cho obj
 var course1 = {
     // name: name,
     // price: price
     name,
     price, // nếu name và key trùng tên có thể viết
 }
+console.log(course);
 
 // method
 var course2 = {
@@ -163,10 +165,147 @@ const course3 = {
 console.log(course);
 
 // 3. Rest parameters
+// rest: phần còn lại
+let array6 = ['Java', 'PHP', 'Ruby'];
+let [x, y, ...rest] = array;
 
+console.log(x); // Java
+console.log(y); // PHP
+console.log(rest); // Ruby
 
-// 4. Spread
+function logger5(...params) {
+    console.log(params);
+}
+logger5(1,2,3,4); // 1,2,3,4
 
+function logger6(a, ...params) {
+    console.log(params);
+}
+logger6(1,2,3,4); // 2,3,4
 
+// truyền 1 obj vào logger
+function logger(obj) {
+    console.log(obj.name);
+    console.log(obj.price);
+}
+logger ({
+    name: 'Javascript',
+    price: 1000,
+})
 
+// cách khác dùng destructuring
+function loggerDes({name, price, ...rest}) { // lấy luôn key muốn lấy ra đặt trong {}
+    console.log(name);
+    console.log(price);
+    console.log(rest); // phần còn lại: description
+}
 
+loggerDes({
+    name: 'Javascript',
+    price: 1000,
+    description: "content"
+})
+
+function logger7([a, b, ...rest]) {
+    console.log(a); // 2
+    console.log(b); // 6
+    console.log(rest); // [3, 4, 4]
+}
+logger7([2, 6, 3, 4, 4]);
+
+// 4. Spread (toán tử dải) (...): dải trước arr hay obj sẽ bỏ đi dấu ngoặc trước sau, chỉ còn phần tử
+let array1 = ['Java', "Ruby", "PHP"];
+let array2 = ['ReactJS', 'Dart'];
+// tạo array3 = nối array1 và array2, array2 trước
+let array3 = [...array2, ...array1]
+console.log(array3); // ['Java', "Ruby", "PHP", "ReactJS", "Dart"]
+
+let obj1 = {
+    name: "Java",
+}
+
+let obj2 = {
+    price: 1000,
+}
+
+let obj3 = {
+    ...obj1,
+    ...obj2
+}
+console.log(obj3); // {name: 'Java', price: 1000}
+
+let defaultConfig = {
+    api:'https://abc',
+    apiVersion: 'v1',
+    other: 'other',
+}
+
+let exerciseConfig = { // muốn thừa hưởng tất cả của defaultConfig
+    ...defaultConfig,
+    api: 'https://abc1', // lấy key api định nghĩa sau => thay đổi link api
+}
+
+// truyền tham số cho hàm dùng spread
+let array4 = ['Java', "Ruby", 'PHP'];
+
+function logger8(a, b, c) { // hoặc dùng rest logger8(...rest)
+    console.log(a, b, c);
+}
+
+logger8(1, 2, 3)
+logger(...array4) // dùng spread
+// Phân biệt rest và spread
+// rest: sử dụng khi kết hợp với destructuring, định nghĩa tham số
+// Spread: truyền đối số (khi gọi đến hàm cần truyền đối số), dải nó trong 1 array hoặc obj
+
+// 5. Destructuring (phân rã): sử dụng với obj và array
+let array5 = ['Java', 'PHP', 'Ruby'];
+// tạo 1 3 biến a, b, c lưu lần lượt các phần tử của array
+// let a = array[0];
+// let b = array[1];
+// let c = array[2];
+// console.log(a, b, c);
+
+// sử dụng destructuring
+let [a, b, c] = array; // đặt tên biến tự do
+console.log(a, b, c); // Java PHP Dart
+
+// lấy phần tử đầu và cuối
+let [d, , e] = array;
+console.log(d, e); // Java Dart
+
+// sử dụng với obj
+let course2 = {
+    name: 'Javascript',
+    price: 2000,
+    image: 'img-address'
+}
+// dùng destructuring lấy ra cả name và price
+var {name, price, image} = course2; // gọi đúng key của obj
+console.log(name, price);
+
+// không lấy ra name
+// var {name, ...rest} = course2;
+// console.log(rest);
+// console.log(name); // Javascript
+
+// Khi obj trùng key
+let course4 = {
+    name: 'Javascript',
+    price: 2000,
+    image: 'img-address',
+    children: {
+        name: 'ReactJS',
+    }
+}
+
+let {name, children} = course4;
+// lấy name của chidren
+// let {name, children: {name}} = course4
+// // do children là 1 obj nên dùng {}
+// console.log(name) // ReactJS => do children.name ghi đè lên course4.name
+
+// cần đổi tên nếu muốn lấy course4.name
+let {name: parentName, children: {name: childrenName}} = course4;
+console.log(parentName); // Javascript
+console.log(childrenName); // ReactJS
